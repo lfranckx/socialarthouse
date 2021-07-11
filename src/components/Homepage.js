87d3953e18f15d04881fc9ba830ec1e181afd506
Bowie <lfranckx@thinkful.com>
2021-07-11 07:18:02 -0500

    update media queries

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import '../styles/Homepage.scss';
 import { Link } from 'react-router-dom';
+
 import ArtistRoster from './ArtistRoster';
 import Partners from './Partners';
+import Services from './Services';
 
 import maria from "../images/maria.jpg";
 import justine from "../images/justine.jpg";
@@ -36,32 +38,7 @@ export default function Homepage(props) {
                     </ul>
                 </section>
 
-                <section id="services">
-                    <h3>Services</h3>
-
-                    <ul>
-                        <li>
-                            <h4>Brand Buildings</h4>
-                            <p>Find, fine-tune, captivate and optimize opportunities within the market that align with your brand’s fingerprint. For up and coming brands, or established brands looking to grow, our brand building services will provide the foundational insights to help you build your dream. Let us do the heavy lifting.</p>
-                            <button>View More <i class="fas fa-chevron-right"></i></button>
-                        </li>
-                        <li>
-                            <h4>Platform &amp; Media Design</h4>
-                            <p>Connect with your customers and attract new ones by optimizing your media and retail platform functionality and incorporating an engaging customer experience. Let’s conceptualise, collaborate and tell your story through the build of reinvisioned media.</p>
-                            <button>View More <i class="fas fa-chevron-right"></i></button>
-                        </li>
-                        <li>
-                            <h4>Educational Development Services</h4>
-                            <p>Formulating and curating. From helping to develop product formulations to co-writing your unique education curriculum, Social Art House has a collective 20+ years of experience developing education for the industries most influential global product launches and talent.</p>
-                            <button>View More <i class="fas fa-chevron-right"></i></button>
-                        </li>
-                        <li>
-                            <h4>Shows, Events, &amp; Production</h4>
-                            <p>Your worth your weight in gold. Let us help monetize your educaucation and concepts and bring them to life, virtually or on-location. From event and show development, technical production, or management of all of the above, we support you and your vision–where there’s a will there’s a way… no job is too small, no dream too big.</p>
-                            <button>View More <i class="fas fa-chevron-right"></i></button>
-                        </li>
-                    </ul>
-                </section>
+                <Services />
 
                 <section id="contact">
                     <h3>Need to Connect?</h3>
