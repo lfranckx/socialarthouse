5210db3ac0a15d06d4fb4e4ba11bb73c3d190e82
Bowie <lfranckx@thinkful.com>
2021-07-13 02:08:30 -0500

    remove fader component

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header(props) {
         <header id="header">
             <h2>Build Your Dream</h2>
             <h3>We Offer an Expansive Consulting Experience for Brands Through <span>Ingenuity, Originality, and Imagination</span></h3>
-            
+
             <Link activeClass="active" to="about" spy={true} smooth={true} offset={0} duration={500}>
                 <i className="fas fa-chevron-down bounce"></i>
             </Link>
