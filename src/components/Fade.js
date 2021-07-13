aeda14ef3aa72f2b6f1aa2308161a771a136c7ad
Bowie <lfranckx@thinkful.com>
2021-07-13 01:49:40 -0500

    start fade in component

---
+++ src/components/Fade.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+
+import PropTypes from 'prop-types';
+
+const Fade = ({ text }) {
+    const [fadeProp, setFadeProp] = useState({
+        fade: 'fade-in'
+    });
+
+    useEffect(() => {
+        effect
+        return () => {
+            cleanup
+        }
+    }, [input])
+}
