9f26d4bc6902500de63ed23af2fb602a13ce3668
Bowie <lfranckx@thinkful.com>
2021-07-17 13:22:40 -0500

    add react-helmt to home and contact page

--- src/App.js
+++ src/App.js
@@ -8,20 +8,28 @@ import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
 import ThankYou from './components/ThankYouPage';
 import NotFoundPage from './components/NotFoundPage';
+import { Helmet } from 'react-helmet';
 
 
 function App() {
   return (
-    <div className="App">
-        <Navbar id='navbar'/>
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route exact path='/contact' component={ContactForm} />
-          <Route exact path='/thankyou' component={ThankYou} />
-          <Route component={NotFoundPage} />
-        </Switch>
-        <Footer />
-    </div>
+    <>
+      <Helmet >
+        <title>Social Arthouse</title>
+        <meta name='description' content='A pro-beauty management and creative agency, offering an expansive consulting experience for brands through ingenuity, originality and imagination.'/>
+        <meta name='keywords' content='Beauty Management, Creative Agency, Consulting Firm' />
+      </Helmet>
+      <div className="App">
+          <Navbar id='navbar'/>
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route exact path='/contact' component={ContactForm} />
+            <Route exact path='/thankyou' component={ThankYou} />
+            <Route component={NotFoundPage} />
+          </Switch>
+          <Footer />
+      </div>
+    </>
   );
 }
 
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useHistory } from 'react-router';
 import { useSpring, animated } from 'react-spring';
+import { Helmet } from 'react-helmet';
 import emailjs from 'emailjs-com';
 
 import '../styles/ContactForm.scss';
@@ -26,86 +27,93 @@ const ContactForm = () => {
     const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } })
 
     return(
-        <main> 
-            <animated.div style={props} id="contact-form">
-                <div className='container'>
-                    <h2>Get in Touch</h2>
-                    <h3>Expansive Consulting Experience for Brands through ingenuity, originality, and imagination</h3>
-                </div>
-                <form onSubmit={sendEmail}>
-                    <div className="label">
-                        <label htmlFor="name"></label>
+        <>
+            <Helmet >
+                <title>Contact Social Arthouse</title>
+                <meta name='description' content='Get in touch with our team'/>
+                <meta name='keywords' content='Beauty Management, Creative Agency, Consulting Firm' />
+            </Helmet>
+            <main> 
+                <animated.div style={props} id="contact-form">
+                    <div className='container'>
+                        <h2>Get in Touch</h2>
+                        <h3>Expansive Consulting Experience for Brands through ingenuity, originality, and imagination</h3>
                     </div>
-                    <div>
-                        <input
-                            type="text"
-                            name="name"
-                            placeholder='Your Name'
-                        />
-                    </div>
+                    <form onSubmit={sendEmail}>
+                        <div className="label">
+                            <label htmlFor="name"></label>
+                        </div>
+                        <div>
+                            <input
+                                type="text"
+                                name="name"
+                                placeholder='Your Name'
+                            />
+                        </div>
 
-                    <div className="label">
-                        <label htmlFor="salon"></label>
-                    </div>
-                    <div>
-                        <input
-                            type="text"
-                            name="salon"
-                            placeholder='Your Salon'
-                        />
-                    </div>
+                        <div className="label">
+                            <label htmlFor="salon"></label>
+                        </div>
+                        <div>
+                            <input
+                                type="text"
+                                name="salon"
+                                placeholder='Your Salon'
+                            />
+                        </div>
 
-                    <div className="label">
-                        <label htmlFor="email"></label>
-                    </div>
-                    <div>
-                        <input
-                            type="email"
-                            name="email"
-                            placeholder="Email"
-                        />
-                    </div>
+                        <div className="label">
+                            <label htmlFor="email"></label>
+                        </div>
+                        <div>
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder="Email"
+                            />
+                        </div>
 
-                    <div className="label">
-                        <label htmlFor="phone"></label>
-                    </div>
-                    <div>
-                        <input
-                            type="tel"
-                            name="phone"
-                            placeholder='Your Phone Number'
-                        />
-                    </div>
+                        <div className="label">
+                            <label htmlFor="phone"></label>
+                        </div>
+                        <div>
+                            <input
+                                type="tel"
+                                name="phone"
+                                placeholder='Your Phone Number'
+                            />
+                        </div>
 
-                    <div className="label">
-                        <label htmlFor="website"></label>
-                    </div>
-                    <div>
-                        <input
-                            type="text"
-                            name="website"
-                            placeholder='Your Website or Instagram'
-                        />
-                    </div>
+                        <div className="label">
+                            <label htmlFor="website"></label>
+                        </div>
+                        <div>
+                            <input
+                                type="text"
+                                name="website"
+                                placeholder='Your Website or Instagram'
+                            />
+                        </div>
 
-                    <div className="label">
-                        <label htmlFor="message"></label>
-                    </div>
+                        <div className="label">
+                            <label htmlFor="message"></label>
+                        </div>
 
-                    <div>
-                        <textarea 
-                            name='message' 
-                            rows='10' 
-                            placeholder="Describe how you would like to work with us"
-                        />
-                    </div>
-                    
-                    <button className="btn" type="submit">
-                        Send
-                    </button>
-                </form>
-            </animated.div>
-        </main>
+                        <div>
+                            <textarea 
+                                name='message' 
+                                rows='10' 
+                                placeholder="Describe how you would like to work with us"
+                            />
+                        </div>
+                        
+                        <button className="btn" type="submit">
+                            Send
+                        </button>
+                    </form>
+                </animated.div>
+            </main>
+        </>
     )
 }
 
