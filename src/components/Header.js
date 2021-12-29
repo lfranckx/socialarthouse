fab31ccd4469217126d9293b7e9fbd8be6b1f6ff
Bowie <lfranckx@thinkful.com>
2021-12-29 11:15:12 -0600

    add header video

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,10 @@ export default function Header() {
         
         <animated.div style={props}>
             <header id="header">
+                <video autoPlay muted loop id="homeVideo">
+                    <source src="videos/Black.mp4" type="video/mp4"/>
+                    Your browser does not support HTML5 video.
+                </video>
                 <h2>Build Your Dream</h2>
                 <h3>We Offer an Expansive Consulting Experience for Brands Through <span>Ingenuity, Originality, and Imagination</span></h3>
 
