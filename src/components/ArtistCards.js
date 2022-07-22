bce0e19165399aa773e4a0394c44e40490525b2c
Bowie <lfranckx@thinkful.com>
2022-07-21 20:10:35 -0500

    update artists

--- src/components/ArtistCards.js
+++ src/components/ArtistCards.js
@@ -1,38 +1,9 @@
 const ArtistCards = [
     {
         url: 'https://www.instagram.com/johnnyramirez/',
-        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/johnny.jpg',
+        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/2022_artist_roster/johnny-ramirez.jpg',
         name: 'Johnny Ramirez',
         ig: '@johnnyramirez'
-    },
-    {
-        name: 'block',
-        color: 'grey',
-        height: 'small'
-    },
-    {
-        url: 'https://www.instagram.com/leysahairandmakeup/',
-        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/leysa.png',
-        name: 'Leysa Carrillo',
-        ig: '@leysahairandmakeup'
-    },
-    {
-        url: 'https://www.instagram.com/michelleoconnorbeauty/',
-        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/michelle.jpg',
-        name: `Michelle O'Connor`,
-        ig: '@michelleoconnorbeauty'
-    },
-    {
-        url: 'https://www.instagram.com/shelleygregoryhair/',
-        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/shelley.jpg',
-        name: 'Shelley Gregory',
-        ig: '@shelleygregoryhair'
-    },
-    {
-        url: 'https://www.instagram.com/tippishorter/',
-        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/tippi-cropped.jpg',
-        name: 'Tippi Shorter',
-        ig: '@tippishorter'
     },
     {
         name: 'block',
@@ -40,33 +11,78 @@ const ArtistCards = [
         height: 'medium'
     },
     {
-        url: 'https://www.instagram.com/chitabeseau/',
-        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/chita.png',
-        name: 'Chita Beseau',
-        ig: '@chitabeseau'
-    },
-    {
-        url: 'https://www.instagram.com/monaeartistry/',
-        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/monae-everett.jpeg',
-        name: 'Monae Everett',
-        ig: '@monaeartistry'
-    },
-    {
-        url: 'https://www.instagram.com/sydneyannlopezhair/',
-        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/sydney.png',
-        name: 'Sydney Lopez',
-        ig: '@sydneyannlopezhair'
+        url: 'https://www.instagram.com/leysahairandmakeup/',
+        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/2022_artist_roster/leysa-carrillo.png',
+        name: 'Leysa Carrillo',
+        ig: '@leysahairandmakeup'
     },
     {
         name: 'block',
-        color: 'orange',
+        color: 'black',
         height: 'large'
     },
     {
         url: 'https://www.instagram.com/sonnabrado/',
-        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/sonna.png',
-        name: 'Sonna Brado',
+        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/2022_artist_roster/sonna-bronado.jpg',
+        name: `Sonna Brado`,
         ig: '@sonnabrado'
+    },
+    {
+        name: 'block',
+        color: 'orange',
+        height: 'medium'
+    },
+    {
+        url: 'https://www.instagram.com/jackhowardcolor/',
+        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/2022_artist_roster/jack-howard.jpeg',
+        name: `Jack Howard`,
+        ig: '@jackhowardcolor'
+    },
+    {
+        url: 'https://www.instagram.com/wesdoeshair/',
+        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/wes.png',
+        name: 'Wes Palmer',
+        ig: '@wesdoeshair'
+    },
+    {
+        name: 'block',
+        color: 'brown',
+        height: 'small'
+    },
+    {
+        url: 'https://www.instagram.com/chitabeseau/',
+        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/2022_artist_roster/chita-beseau.jpeg',
+        name: 'Chita Beseau',
+        ig: '@chitabeseau'
+    },
+    {
+        url: 'https://www.instagram.com/michelleoconnorbeauty/',
+        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/2022_artist_roster/michelle-oconnor.jpeg',
+        name: `Michelle O'Connor`,
+        ig: '@michelleoconnorbeauty'
+    },
+    {
+        url: 'https://www.instagram.com/derickmonroe/',
+        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/2022_artist_roster/derick-monroe.png',
+        name: 'Derick Monroe',
+        ig: '@derickmonroe'
+    },
+    {
+        name: 'block',
+        color: 'grey',
+        height: 'small'
+    },
+    {
+        url: 'https://www.instagram.com/sydneyannlopezhair/',
+        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/sydney.png',
+        name: 'Sydney Ann Lopez',
+        ig: '@sydneyannlopezhair'
+    },
+    {
+        url: 'https://www.instagram.com/tippishorter/',
+        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/2022_artist_roster/tippi-shorter.jpeg',
+        name: 'Tippi Shorter',
+        ig: '@tippishorter'
     },
     {
         url: 'https://www.instagram.com/minkimcolorist/',
@@ -76,31 +92,59 @@ const ArtistCards = [
     },
     {
         name: 'block',
-        color: 'grey',
+        color: 'orange',
         height: 'medium'
     },
     {
-        url: 'https://www.instagram.com/wesdoeshair/',
-        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/wes.png',
-        name: 'Wes Palmer',
-        ig: '@wesdoeshair'
+        url: 'https://www.instagram.com/monaeartistry/',
+        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/monae-everett.jpeg',
+        name: 'Monae Everett',
+        ig: '@monaeartistry'
     },
     {
         url: 'https://www.instagram.com/lgibsoncolorist/',
-        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/laura.png',
+        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/2022_artist_roster/laura-gibson.png',
         name: 'Laura Gibson',
         ig: '@lgibsoncolorist'
+    },
+    {
+        name: 'block',
+        color: 'brown',
+        height: 'medium'
+    },
+    {
+        url: 'https://www.instagram.com/rolandoaqui/',
+        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/2022_artist_roster/rolando-aqui.jpg',
+        name: 'Rolando Aqui',
+        ig: '@rolandoaqui'
     },
     {
         name: 'block',
         color: 'black',
         height: 'medium'
     },
-
+    {
+        url: 'https://www.instagram.com/slickback_buttahtoast/',
+        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/2022_artist_roster/ethan-king.jpeg',
+        name: 'Ethan King',
+        ig: '@slickback_buttahtoast'
+    },
+    {
+        url: 'https://www.instagram.com/irapopesage/',
+        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/2022_artist_roster/ira-pope-sage.jpg',
+        name: 'Ira Pope King',
+        ig: '@irapopesage'
+    },
+    {
+        url: 'https://www.instagram.com/ktbhair/',
+        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/2022_artist_roster/katie-brenner.jpg',
+        name: 'Katie Brenner',
+        ig: '@ktbhair'
+    },
     {
         name: 'block',
-        color: 'brown',
-        height: 'medium'
+        color: 'grey',
+        height: 'small'
     },
     {
         url: 'https://www.instagram.com/ramontgarcia/',
@@ -109,26 +153,26 @@ const ArtistCards = [
         ig: '@ramontgarcia'
     },
     {
+        url: 'https://www.instagram.com/jamiewileyeditorial/',
+        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/2022_artist_roster/jamie-wiley.jpeg',
+        name: 'Jamie Wiley',
+        ig: '@jamiewileyeditorial'
+    },
+    {
+        url: 'https://www.instagram.com/__yure_style__/',
+        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/2022_artist_roster/yureesh-hooker.png',
+        name: 'Yureesh Hooker',
+        ig: '@__yure_style__'
+    },
+    {
         name: 'block',
-        color: 'orange',
+        color: 'brown',
         height: 'medium'
     },
     {
-        url: 'https://www.instagram.com/rolandoaqui/',
-        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/rolando.jpg',
-        name: 'Rolando Aqui',
-        ig: '@rolandoaqui'
-    },
-    {
-        url: 'https://www.instagram.com/derickmonroe/',
-        src: 'https://socialarthouse.s3.us-east-2.amazonaws.com/Artists/derik.png',
-        name: 'Derick Monroe',
-        ig: '@derickmonroe'
-    },
-    {
         name: 'block',
-        color: 'grey',
-        height: 'large'
+        color: 'orange',
+        height: 'medium'
     },
     {
         name: 'block',
